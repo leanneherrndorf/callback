cfeca1a2a4e490271e7270dcbc7bf95349dafaff
Leanne <leanneherrndorf@gmail.com>
2017-03-09 17:39:17 +0000

    Modified findWaldo to be a forEach method

--- findWaldo.js
+++ findWaldo.js
@@ -1,18 +1,16 @@
-var index = 0;
+var foundIndex = 0;
 
 // The second argument/parameter is expected to be a function
-function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      index = i;
-      found();   // execute callback
 
+var findWaldo = function(element, index) {
+   if (element === "Waldo") {
+      foundIndex = index;
+      return actionWhenFound();   // execute callback
     }
-  }
 }
 
 function actionWhenFound() {
-  console.log("Found Waldo at index " + index + "!");
+  console.log("Found Waldo at index " + foundIndex + "!");
 }
 
-findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+["Alice", "Bob", "Waldo", "Winston"].forEach(findWaldo);
